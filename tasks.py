0db50e1489172484e350137bb0a2ea57d4aaf36b
Sebastian Rust <harlequix@gmail.com>
2021-08-06 21:15:05 +0200

    added new lines

--- tasks.py
+++ tasks.py
@@ -6,28 +6,54 @@ import logging as log
 import regex as re
 from eval.utils import parse_folder_name
 from pprint import pprint
-
+from eval.validate_logs import validate_logfile
+import argparse
+import multiprocessing
 
 log.basicConfig(format='%(levelname)s:%(message)s', level=log.INFO)
 
-DATA = pathlib.Path("./data/")
+DATA = pathlib.Path("./data")
 
-RUNBASE = pathlib.Path("./runs/")
+parser = argparse.ArgumentParser()
+parser.add_argument("--runbase", default="./runs/")
+args, _ = parser.parse_known_args()
+
+
+RUNBASE = pathlib.Path(args.runbase)
 LOGS = "logs"
 STATS = "stats"
 RESULTS = "results"
 
 
 @task
-def cleanup(ctx):
-    cmd = f"grep -r  'Test run stuck for too long, abort' ./runs"
-    run(cmd)
+def metrics(ctx, name, runbase=RUNBASE):
+    stats(ctx, name, runbase=runbase)
+    throughput(ctx, name, runbase=runbase)
+    walltime(ctx, name, runbase=runbase)
+    overhead(ctx, name, runbase=runbase)
+    accuracy(ctx, name, runbase=runbase)
 
 
 @task
-def build(ctx):
-    print("Building!")
+def cleanup(ctx, paths="all"):
+    runs = get_paths(paths)
+    for run in runs:
+        log.disable("WARN")
+        logs = (run/LOGS).glob("*.log")
+        faulty = [x for x in logs if not validate_logfile(x)]
+        if faulty:
+            print(run)
+            for f in faulty:
+                print(f"\t{f} is faulty")
 
+
+@task
+def avgtput(ctx, paths, rebuild=False, runbase=RUNBASE):
+    runs = get_paths(paths)
+    for r in runs:
+        cmd = f"python eval/avg_tput.py {r}"
+        print(cmd)
+        run(cmd)
 
 @task
 def init(ctx, name):
@@ -68,7 +94,7 @@ def update(ctx, paths="all"):
 
 
 @task()
-def stats(ctx, paths, rebuild=False):
+def stats(ctx, paths, rebuild=False, runbase=RUNBASE):
     runs = get_paths(paths)
     # print(runs)
     for runP in runs:
@@ -86,22 +112,41 @@ def stats(ctx, paths, rebuild=False):
 
 
 @task
-def throughput(ctx, name):
+def throughput(ctx, name, runbase=RUNBASE):
     runs = get_paths(name)
     for r in runs:
         touch(r)
-        stats = (r/STATS).glob("*.csv")
-        for stat in stats:
-            cmd = f"python eval/throughput.py  {stat} {r/RESULTS/(f'throughput-{stat.name}')}"
+        rxlogs = (r/LOGS).glob("*rx.log")
+        txlogs = (r/LOGS).glob("*tx.log")
+        rxlogs = sorted([x for x in rxlogs])
+        txlogs = sorted([x for x in txlogs])
+        logs_filtered = [x for x in zip(rxlogs, txlogs) if is_valid(x)]
+        rxinputs = " ".join([str(x[0]) for x in logs_filtered])
+        txinputs = " ".join([str(x[1]) for x in logs_filtered])
+        rxcmd = f"python eval/throughput-rx.py --inputs {rxinputs} --output {r/RESULTS/'throughput-rx.csv'}"
+        txcmd = f"python eval/throughput-tx.py --inputs {txinputs} --output {r/RESULTS/'throughput-tx.csv'}"
+
+        for cmd in [txcmd, rxcmd]:
             try:
                 run(cmd)
             except Exception as e:
-                print(stat)
+                print(cmd)
                 print(e)
 
 
 @task
-def walltime(ctx, name, rebuild=False):
+def overhead(ctx, name, runbase=RUNBASE):
+    runs = get_paths(name)
+    for r in runs:
+        cmd = f"python eval/overhead.py {r}"
+        try:
+            run(cmd)
+        except Exception as e:
+            log.warn(f"run:{r}, cmd:{cmd}, error:{e}")
+
+
+@task
+def walltime(ctx, name, runbase=RUNBASE, rebuild=False):
     runs = get_paths(name)
     for r in runs:
         touch(r)
@@ -116,18 +161,21 @@ def walltime(ctx, name, rebuild=False):
             except Exception as e:
                 log.warn(f"run:{r}, cmd:{cmd}, error:{e}")
 
+
 @task
-def prep_accuracy(ctx, name, rebuild=False):
+def accuracy(ctx, name, runbase=RUNBASE, rebuild=False):
     experiments = get_paths(name)
     for exp in experiments:
         stats = (exp/STATS).glob("*.csv")
-        acc_result = (exp/RESULTS/"accuracy")
-        acc_result.mkdir(parents=True, exist_ok=True)
-        for stat in stats:
-            output = acc_result/f"accuracy-{stat.name}"
-            cmd = f"python eval/accuracy.py {stat} {output}"
-            run(cmd)
-
+        acc_result = (exp/RESULTS/"accuracy.csv")
+        # acc_result.mkdir(parents=True, exist_ok=True)
+        paths = " ".join([str(x) for x in stats])
+        if paths != " ":
+            cmd = f"python eval/accuracy.py --input {paths} --output {acc_result}"
+            try:
+                run(cmd)
+            except Exception as e:
+                log.warn(f"run:{exp}, cmd:{cmd}, error:{e}")
 
 
 def get_paths(name):
@@ -140,7 +188,7 @@ def get_paths(name):
 
 def pair_up(path):
     pairs = {}
-    files = path.glob("*")
+    files = path.glob("*.log")
     for file in files:
         log.debug(file)
         name = file.name
