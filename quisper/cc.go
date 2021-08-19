63aaeb537378533550d525a2cc2b52daafeb2eac
Sebastian Rust <harlequix@gmail.com>
2021-08-19 12:04:06 +0200

    fixed: tcp vegas algorithm

--- quisper/cc.go
+++ quisper/cc.go
@@ -51,37 +51,45 @@ func (self *CCVegas) adjust (lastTimeSlot uint64) int{
     } else {
         minRTTi := self.rtts.GetMinRTT().Nanoseconds()
         currentRTTi := self.rtts.GetMeasurement().Nanoseconds()
-        alpha := 0.3
-        beta := 0.6
+        alpha := float64(1)
+        beta := float64(3)
         minRTT := float64(minRTTi)
         currentRTT := float64(currentRTTi)
-        lower_limit := minRTT + minRTT * alpha
-        upper_limit := minRTT + minRTT * beta
+        cwnd := float64(self.bucketSize)
+        expected := cwnd/minRTT
+        actual := cwnd/currentRTT
+
+        diff := (expected - actual)*currentRTT
+        // lower_limit := minRTT + minRTT * alpha
+        // upper_limit := minRTT + minRTT * beta
         self.logger.WithFields(logrus.Fields{
             "minRTT": minRTT,
             "currentRTT": currentRTT,
-            "upper_limit": upper_limit,
-            "lower_limit": lower_limit,
+            "expected": expected,
+            "actual": actual,
+            "diff": diff,
             "bucketSize": self.bucketSize,
         }).Trace("Adjusting bucketSize")
-        if currentRTT < lower_limit {
+        if diff < alpha {
             self.bucketSize++
             self.logger.WithFields(logrus.Fields{
                 "minRTT": minRTT,
                 "currentRTT": currentRTT,
-                "upper_limit": upper_limit,
-                "lower_limit": lower_limit,
+                "expected": expected,
+                "actual": actual,
+                "diff": diff,
                 "bucketSize": self.bucketSize,
             }).Trace("Increasing bucketSize")
         }
-        if currentRTT > upper_limit {
+        if diff > beta {
             if self.bucketSize > 1 {
                 self.bucketSize--
                 self.logger.WithFields(logrus.Fields{
                     "minRTT": minRTT,
                     "currentRTT": currentRTT,
-                    "upper_limit": upper_limit,
-                    "lower_limit": lower_limit,
+                    "expected": expected,
+                    "actual": actual,
+                    "diff": diff,
                     "bucketSize": self.bucketSize,
                 }).Trace("Decreasing bucketSize")
             }
@@ -89,3 +97,49 @@ func (self *CCVegas) adjust (lastTimeSlot uint64) int{
         return self.bucketSize
     }
 }
+
+// type CCTargetControl struct {
+//     logger *log.Logger
+//     rtts rtt.Manager
+//     bucketSize int
+//     rrt0Est time.Duration
+//     lastTimeSlot uint64
+// }
+//
+// func NewCCTargetControl(initialSize int, rtts rtt.Manager)*CCTargetControl{
+//     ccontroller := &CCTargetControl{
+//         logger: log.NewLogger("CCVegas"),
+//         rtts: rtts,
+//         bucketSize: initialSize,
+//     }
+//     return ccontroller
+// }
+//
+// func (self *CCTargetControl) CanExpand(timeslot uint64) bool{
+//     last := self.lastTimeSlot
+//     self.lastTimeSlot = lastTimeSlot
+//     if last == 0 {
+//         self.lastTimeSlot = lastTimeSlot
+//         return self.bucketSize
+//     }
+//     if lastTimeSlot - last > 1 {
+//         return self.bucketSize
+//     } else {
+//         minRTTi := self.rtts.GetMinRTT()
+//         smoothRtt := self.rtts.GetMeasurement()
+//         target := self.calculateTarget(minRTTi)
+//         self.logger.WithFields(logrus.Fields{
+//             "minrtt":minRTTi,
+//             "smoothRtt": smoothRtt,
+//             "target": target,
+//             }).Debug("Calculating new buckets")
+//     }
+// }
+//
+// func (self *CCTargetControl) calculateTarget(minrtt time.Duration) time.Duration {
+//     stepHigher := time.Duration(200 * time.Milliseconds())
+    // while (stepHigher.Nanoseconds() < minrtt.Nanoseconds()){
+    //     // stepHigher = stepHigher * 2
+    // }
+    // return stepHigher - time.Duration(100 * time.Milliseconds())
+// }
